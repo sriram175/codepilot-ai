ee1e39eb01757e8da7df191838154cf33da3e3b4
Sriram Mallina <sriram.mallina059@gmail.com>
2026-08-10 22:11:38 +0530

    "Pre deployment changes has been done"

--- codepilot-ai/src/main/java/com/sriram/ai/codepilot_ai/ingestion/Qdrant/QdrantConfiguration.java
+++ codepilot-ai/src/main/java/com/sriram/ai/codepilot_ai/ingestion/Qdrant/QdrantConfiguration.java
@@ -1,21 +1,27 @@
 package com.sriram.ai.codepilot_ai.ingestion.Qdrant;
 
-
 import io.qdrant.client.QdrantClient;
 import io.qdrant.client.QdrantGrpcClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class QdrantConfiguration {
 
+    @Value("${QDRANT_HOST}")
+    private String host;
+
+    @Value("${QDRANT_PORT}")
+    private int port;
+
     @Bean
     public QdrantClient qdrantClient() {
 
         return new QdrantClient(
                 QdrantGrpcClient.newBuilder(
-                        "localhost",
-                        6334,
+                        host,
+                        port,
                         false
                 ).build()
         );
