fc48ef0fc4b4b7e54199d3885b6918a23b2206ed
Sriram Mallina <sriram.mallina059@gmail.com>
2026-08-15 16:04:12 +0530

    "Modified the conversation deletion logic"

--- codepilot-ai/src/main/java/com/sriram/ai/codepilot_ai/repository/MessageRepository.java
+++ codepilot-ai/src/main/java/com/sriram/ai/codepilot_ai/repository/MessageRepository.java
@@ -9,4 +9,5 @@ import java.util.List;
 @Repository
 public interface MessageRepository extends JpaRepository<Message,Long> {
     List<Message> findByConversationIdOrderByCreatedAtAsc(Long conversationId);
+    void deleteByConversationId(Long conversationId);
 }
